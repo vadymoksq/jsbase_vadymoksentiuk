37c0d31bafff01115822c026a2f7632eb465ccdc
vadymoksq <vadimoksq@gmail.com>
2021-05-31 18:44:11 +0300

    aboba

---
+++ HW1/js/task4.js
@@ -0,0 +1,9 @@
+//   Получить от пользователя данные о пользователе: имя, фамилию, возраст. Обработать их и вывести на экран.
+
+var firsname = prompt("Введите Ваше имя");
+var lastname = prompt("Введите Вашу фамилию");
+var age = prompt("Введите Ваш возраст");
+
+document.write("Имя: " + firsname + "<br>");
+document.write("Фамилия: " + lastname + "<br>");
+document.write("Возраст: " + age);
